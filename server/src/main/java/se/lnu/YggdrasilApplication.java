2006cf18562cb097403c9243e9fd43e63b0c0932
John Herrlin <jherrlin@gmail.com>
2016-01-05 18:23:09 +0100

    Spring boot started

---
+++ server/src/main/java/se/lnu/YggdrasilApplication.java
@@ -0,0 +1,12 @@
+package se.lnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YggdrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YggdrasilApplication.class, args);
+	}
+}
