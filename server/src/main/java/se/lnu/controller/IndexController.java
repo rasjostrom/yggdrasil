b9c57ed9383f05d9f68b343617336f83624ee9f0
John Herrlin <jherrlin@gmail.com>
2016-01-09 20:06:10 +0100

    Basic Spring Boot and AngularJS added

---
+++ server/src/main/java/se/lnu/controller/IndexController.java
@@ -0,0 +1,24 @@
+package se.lnu.controller;
+
+/**
+ * Created by nils on 09/01/16.
+ */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/resource")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+}
