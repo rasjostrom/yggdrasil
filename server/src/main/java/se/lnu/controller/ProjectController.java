81ef54edd25c982eaa870b52e6475feb10793fde
John Herrlin <jherrlin@gmail.com>
2016-01-09 20:54:49 +0100

    gitignore updated

---
+++ server/src/main/java/se/lnu/controller/ProjectController.java
@@ -0,0 +1,7 @@
+package se.lnu.controller;
+
+/**
+ * Created by nils on 09/01/16.
+ */
+public class ProjectController {
+}
---
+++ server/src/main/java/se/lnu/domain/Feature.java
@@ -0,0 +1,7 @@
+package se.lnu.domain;
+
+/**
+ * Created by nils on 09/01/16.
+ */
+public class Feature {
+}
