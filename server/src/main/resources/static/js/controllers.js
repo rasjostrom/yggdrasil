0bf08fe23d3953d4237abb75925b1e61dd2500f3
rasmus <ras.sjostrom@hotmail.com>
2016-01-11 01:51:18 +0100

    Add delete function for projects

--- server/src/main/resources/static/js/controllers.js
+++ server/src/main/resources/static/js/controllers.js
@@ -2,14 +2,15 @@ angular.module('yggdrasil.controllers', [])
 
 .controller('sidenavCtrl', function($scope, $http, $mdDialog) {
     var allProjects = [];
-
+    
     $scope.currentProject = {title: 'Yggdrasil',
 			     description: 'Description here',
 			     features: [{description: "List all Projects", complete: true},
 					{description: "Comment on project wall", complete: false}]};
 
     $scope.currentIssues = [];
-    
+
+    // Fetch all projects from DB and save to 'allProjects'
     $http({
 	method: 'GET',
 	url: '/projects'
@@ -20,16 +21,32 @@ angular.module('yggdrasil.controllers', [])
 	console.log("Error fetching data from DB.");
     });
 
+    // Update the content view with data from a selected project
     $scope.displayProject = function(project) {
 	$scope.currentProject = project;
 	console.log(project);
     };
 
+    // Update the issues view with data from a selected feature
     $scope.displayIssues = function(issues) {
 	$scope.currentIssues = issues;
 	console.log(issues);
     };
-   
+
+    // Remove a project
+    $scope.deleteProject = function(project, $window) {
+	var projectId = project.id;
+	var deleteUrl = "/projects/" + projectId;
+	console.log(deleteUrl);
+	$http.delete(deleteUrl).then(function successCallback(response) {
+	    $window.location.reload();
+	    console.log("delete successful");
+	}, function errorCallback(response) {
+	    console.log("Error while deleting project.");
+	});
+    };
+    
+   // Show 'new project' dialog
   $scope.showDialog = showDialog;
   function showDialog($event) {
     var parentEl = angular.element(document.body);
@@ -41,6 +58,7 @@ angular.module('yggdrasil.controllers', [])
     });
   }
 
+    // Controller for the 'new project' dialog
     function DialogController($scope, $mdDialog, $http, $window) {
       $scope.project = {
 	  title: ' ',
@@ -48,7 +66,7 @@ angular.module('yggdrasil.controllers', [])
 	  features: [],
 	  comments: []
       };
-      
+      // Creates a new project and posts it to the backend
       $scope.create = function(project) {
 	  console.log(project.title);
 	  console.log(project.description);
